c78cf8b5785744a9d92b25df342a49168163b529
subhanzakizada <zakizadasubhan@gmail.com>
2021-02-09 13:42:10 -0800

    display location data and time

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 class App {
-    // API Url - http://api.weatherstack.com/ 
-    // API Key 
-    #apiKey = 'bee46877a770c74747524a7443ff4c04'
+    constructor() {
+        this.displayDataLocationAndTime()
+        this.displayIcon()
+    }
+  
     dataWeatherGlobal
 
-    // Getting Data
+    // Getting Weather Data -- API Url - http://api.weatherstack.com/  
     async getWeatherData() {
         try {
             // pulling out data from API
-            const request = await fetch(`http://api.weatherstack.com/current?access_key=${this.#apiKey}&query= 98004`)
+            const request = await fetch(`http://api.weatherstack.com/current?access_key=${this.APIKEY}&query= 98004`)
             const data = await request.json()
             this.dataWeatherGlobal = {
                 ...data
@@ -40,26 +42,61 @@ class App {
                 } = data.coords
                 resolve([latitude, longitude])
             }, function () {
-                reject(alert('error'))
+                // eject(alert('error'))
             })
         })
 
     async convertLocationData() {
-        // return longitude & latitude
+        // contains [latitude, longitude]
         const coords = await this.getCurrentLocation()
-        console.log(coords[0], coords[1])
-        // converting coords to address by using geocoding
-        const apiKey = 'AIzaSyD5hQYLNns7DfUnSqvsCaxT31YNerP2xxw'
+
+        // converting coords to address by using Google Reverse Geocoding
+        const apiKey = APIKEY
         const geocodingURL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords[0]},${coords[1]}&key=${apiKey}`
         const request = await fetch(geocodingURL)
         const data = await request.json()
-        
-        // street, city, state ; zip code, country
-        const address = data.results[4].formatted_address
-        console.log(address)
+
+        // getting address data ex: street, city, state ; zip code, country
+        const address = await data.results[4].formatted_address
+        return address
+    }
+
+    // Displays Current Time(user's time zone) and Current Location on UI
+    async displayDataLocationAndTime() {
+        // date & time elements
+        const date = document.getElementById('date')
+        const currentTime = document.getElementById('time')
+
+        // date & time variables
+        let now, month, day, hour, minute, second
+
+        // helper function - gets called once in every minute
+        function getCurrentTime() {
+            // date & time variables based on ${now} variable
+            now = new Date()
+            month = now.getMonth()
+            day = now.getDate()
+            hour = now.getHours()
+            minute = now.getMinutes()
+            second = now.getSeconds()
+        }
+        getCurrentTime()
+
+        // if second is 59 -- call helper function -- I found this easier than checking and setting (Month, Day, Hour) - each one by one
+        setInterval(() => {
+            if (second === 59) {
+                getCurrentTime()
+                currentTime.textContent = `${hour} : ${minute}`
+            } else {
+                currentTime.textContent = `${hour} : ${minute}`
+                second++
+            }
+        }, 1000)
+
+        const userLocation = document.getElementById('location') // element
+        const location = await this.convertLocationData() // data 
+        // setting the location data
+        userLocation.textContent = `${location}`
     }
 }
 const app = new App()
-app.displayIcon()
-app.getCurrentLocation()
-app.convertLocationData()
